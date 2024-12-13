9b62b6312e90005ab1933d293b1c6cc7c3da195e
Jenish vagadiya <jenishraghu180@gmail.com>
2024-12-13 20:38:49 +0530

    solved issues

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,18 +1,11 @@
-import { HttpStatus, ErrorCode } from "../error/httpCodes.js";
-import message from "../helper/message.js";
 import services from "../helper/index.js"
-const send = services.sendResponse
 
 
 const asyncHandler = (fn) => {
     return function asyncUtilWrap(req, res, next) {
-        const error = getValidationErrors(req)
-        if (error) {
-            return send(res, HttpStatus.BAD_REQUEST, ErrorCode.BAD_REQUEST_CODE, message.VALIDATION_FAILED, error)
-        } else {
-            const fnReturn = fn(req, res, next)
-            return Promise.resolve(fnReturn).catch(next)
-        }
+        const fnReturn = fn(req, res, next)
+        return Promise.resolve(fnReturn).catch(next)
+
     }
 }
 
